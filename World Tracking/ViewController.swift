df635f602264ecac74bdfbe6ddc295fec98a5d48
Amit Vaidya <amitvaidhya@gmail.com>
2018-07-04 22:35:09 +0530

    Randomly put lot of squares using random function inside add button

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -18,19 +18,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
     }
 
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
 //        decides the height, width and length of the box to be created when add is pressed
         node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        
+        //reflects white light on the surface
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
 //        UIColor.<give any color here> to choose the color of the box
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
-        
-//        move the box around X,Y and Z axis by changing the values (0,0,0) to something else
-        node.position = SCNVector3(0,0,0)
-        
+        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(x,y,z)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     
@@ -43,6 +45,9 @@ class ViewController: UIViewController {
             node.removeFromParentNode()
         }
         self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum,secondNum)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
