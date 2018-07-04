04162373f1a2e51e1122bc966c0a51b1a7d7611b
Amit Vaidya <amitvaidhya@gmail.com>
2018-07-04 22:04:45 +0530

    Added button to put a 3D box at the origin with comments on how to customise it's position, color and edges

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -20,6 +20,19 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration)
     }
 
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+//        decides the height, width and length of the box to be created when add is pressed
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+//        UIColor.<give any color here> to choose the color of the box
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        
+//        move the box around X,Y and Z axis by changing the values (0,0,0) to something else
+        node.position = SCNVector3(0,0,0)
+        
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
